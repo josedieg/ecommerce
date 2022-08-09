e6fe21188913a6e96f6d03892403ddaf9089ec5e
josedieg <diegduarte@hotmail.com>
2022-08-09 18:49:01 -0300

    corrigindo teste

--- src/test/java/com/jd/ecommerce/relacionamento/RelacionamentoManyToOneTest.java
+++ src/test/java/com/jd/ecommerce/relacionamento/RelacionamentoManyToOneTest.java
@@ -9,7 +9,9 @@ import org.junit.Test;
 import com.jd.ecommerce.EntityManagerTest;
 import com.jd.ecommerce.enuns.StatusPedido;
 import com.jd.ecommerce.model.Cliente;
+import com.jd.ecommerce.model.ItemPedido;
 import com.jd.ecommerce.model.Pedido;
+import com.jd.ecommerce.model.Produto;
 
 public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
@@ -30,5 +32,34 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
 	Pedido p = entityManager.find(Pedido.class, pedido.getId());
 	Assert.assertNotNull(p.getClientePedido());
+    }
+    
+    @Test
+    public void verificarRelacionamentoItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(new BigDecimal(111));
+        pedido.setClientePedido(cliente);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        Assert.assertNotNull(itemPedidoVerificacao.getPedido());
+        Assert.assertNotNull(itemPedidoVerificacao.getProduto());
     }
 }
