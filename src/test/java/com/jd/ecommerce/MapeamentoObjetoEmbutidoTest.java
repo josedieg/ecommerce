aa81b996d38249a59b49c6710d05cc22ec3e7510
josedieg <diegduarte@hotmail.com>
2022-08-09 18:49:01 -0300

    corrigindo teste

--- src/test/java/com/jd/ecommerce/MapeamentoObjetoEmbutidoTest.java
+++ src/test/java/com/jd/ecommerce/MapeamentoObjetoEmbutidoTest.java
@@ -41,32 +41,4 @@ public class MapeamentoObjetoEmbutidoTest extends EntityManagerTest {
 	Assert.assertNotNull(p.getEnderecoEntrega().getCep());
     }
     
-    @Test
-    public void verificarRelacionamentoItemPedido() {
-        Cliente cliente = entityManager.find(Cliente.class, 1);
-        Produto produto = entityManager.find(Produto.class, 1);
-
-        Pedido pedido = new Pedido();
-        pedido.setStatus(StatusPedido.AGUARDANDO);
-        pedido.setDataPedido(LocalDateTime.now());
-        pedido.setTotal(new BigDecimal(111));
-        pedido.setClientePedido(cliente);
-
-        ItemPedido itemPedido = new ItemPedido();
-        itemPedido.setPrecoProduto(produto.getPreco());
-        itemPedido.setQuantidade(1);
-        itemPedido.setPedido(pedido);
-        itemPedido.setProduto(produto);
-
-        entityManager.getTransaction().begin();
-        entityManager.persist(pedido);
-        entityManager.persist(itemPedido);
-        entityManager.getTransaction().commit();
-
-        entityManager.clear();
-
-        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
-        Assert.assertNotNull(itemPedidoVerificacao.getPedido());
-        Assert.assertNotNull(itemPedidoVerificacao.getProduto());
-    }
 }
