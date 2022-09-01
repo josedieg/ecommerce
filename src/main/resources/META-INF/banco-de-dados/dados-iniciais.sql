1ee0dc17ec3cfa14989278dd5e17dae3ab4ef79e
josedieg <diegduarte@hotmail.com>
2022-09-01 08:50:58 -0300

    rebase

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,6 +1,8 @@
-insert into produto (id, nome, preco, descricao)values (1, 'telefone ', 499.0, 'Melhor processador');
+-- produto
+insert into produto (id, nome, preco, descricao)values (1, 'telefone', 499.0, 'Melhor processador');
 insert into produto (id, nome, preco, descricao)values (3, 'cadeira', 300.0, 'A mais confortavel do mercado');
 
+-- cliente
 insert into cliente (cliente_id, nome) values (1, 'Jose');
 insert into cliente (cliente_id, nome) values (2, 'Maria');
 
