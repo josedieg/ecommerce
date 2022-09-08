a7b2176b8811803a5dd5a4ee27904d5761005da7
josedieg <diegduarte@hotmail.com>
2022-09-08 08:04:38 -0300

    GerenciamentoTransacoesTest

---
+++ src/test/java/com/jd/ecommerce/entitymanager/GerenciamentoTransacoesTest.java
@@ -0,0 +1,31 @@
+package com.jd.ecommerce.entitymanager;
+
+import org.junit.Test;
+
+import com.jd.ecommerce.EntityManagerTest;
+import com.jd.ecommerce.enuns.StatusPedido;
+import com.jd.ecommerce.model.Pedido;
+
+public class GerenciamentoTransacoesTest extends EntityManagerTest {
+
+    @Test(expected = Exception.class)
+    public void abrirFecharCancelarTransacao() {
+        try {
+            entityManager.getTransaction().begin();
+            metodoDeNegocio();
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    private void metodoDeNegocio() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+        pedido.setStatus(StatusPedido.PAGO);
+
+        if (pedido.getPagamento() == null) {
+            throw new RuntimeException("Pedido ainda não foi pago.");
+        }
+    }
+}
