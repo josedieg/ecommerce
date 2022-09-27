35327096451d814c0d0e1f420653b96092efd8f8
josedieg <diegduarte@hotmail.com>
2022-09-27 07:51:29 -0300

    HerancaTest table per class

--- src/main/java/com/jd/ecommerce/model/Pagamento.java
+++ src/main/java/com/jd/ecommerce/model/Pagamento.java
@@ -1,6 +1,5 @@
 package com.jd.ecommerce.model;
 
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -9,7 +8,6 @@ import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 import com.jd.ecommerce.enuns.StatusPagamento;
 
@@ -19,9 +17,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "dtype")
-@Table(name = "pagamento")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Pagamento extends EntidadeInteger {
 
     @MapsId
--- src/main/java/com/jd/ecommerce/model/PagamentoBoleto.java
+++ src/main/java/com/jd/ecommerce/model/PagamentoBoleto.java
@@ -1,14 +1,16 @@
 package com.jd.ecommerce.model;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@DiscriminatorValue("PagamentoBoleto")
+@Entity
+@Table(name = "pagamento_boleto")
 public class PagamentoBoleto extends Pagamento{
 
     @Column(name = "codigo_barras")
