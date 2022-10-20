b0081dadc7603a97d6b6805400a4a90906614d70
josedieg <diegduarte@hotmail.com>
2022-10-20 11:27:46 -0300

    JoinTest

--- src/test/java/com/jd/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/jd/ecommerce/jpql/JoinTest.java
@@ -11,11 +11,29 @@ import com.jd.ecommerce.EntityManagerTest;
 import com.jd.ecommerce.model.Pedido;
 
 public class JoinTest extends EntityManagerTest {
-	
+
+	@Test
+	public void fazendoJoinFethPedido() {
+		String jpql = "select p from Pedido p join fetch p.itens where p.id = 1";
+
+		TypedQuery<Pedido> query = entityManager.createQuery(jpql, Pedido.class);
+		Pedido pedido = query.getSingleResult();
+		Assert.assertFalse(pedido.getItens().isEmpty());
+	}
+
+	@Test
+	public void fazendoLeftJoinPedidoPagamento() {
+		String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
+
+		TypedQuery<Object[]> query = entityManager.createQuery(jpql, Object[].class);
+		List<Object[]> list = query.getResultList();
+		Assert.assertFalse(list.isEmpty());
+	}
+
 	@Test
 	public void fazendoJoinPedidoPagamento() {
 		String jpql = "select p from Pedido p join p.pagamento pag";
-		
+
 		TypedQuery<Pedido> query = entityManager.createQuery(jpql, Pedido.class);
 		List<Pedido> list = query.getResultList();
 
