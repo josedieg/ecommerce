a3f34cb4f444abb264862a3ed05d46374c9437a4
josedieg <diegduarte@hotmail.com>
2022-11-02 11:49:22 -0300

    LockPessimistaTest

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -7,12 +7,12 @@ insert into cliente (id, versao, nome) values (2, 0, 'Aparecida Lima');
 insert into pedido (id, versao, cliente_id, data_criacao, total, status) values (1, 0, 1, sysdate(), 998.0, 'AGUARDANDO');
 insert into pedido (id, versao, cliente_id, data_criacao, total, status) values (2, 0, 1, sysdate(), 499.0, 'AGUARDANDO');
 
-insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (1, 0, 1, 499, 2);
-insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (2, 0, 1, 499, 1);
+insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 499, 2);
+insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (2, 1, 499, 1);
 
 insert into db_ecommerce.pagamento (dtype,pedido_id,status,numero) values (', 0PagamentoCartao',2,'PROCESSANDO','123123123');
 
-insert into nota_fiscal (pedido_id, xml, data_emissao) values (2, 0, '<xml />', sysdate());
+insert into nota_fiscal (pedido_id, versao, xml, data_emissao) values (2, 0, '<xml />', sysdate());
 
 insert into categoria (id, versao, nome) values (1, 0, 'Eletrônicos');
 insert into categoria (id, versao, nome) values (2, 0, 'Móveis');
